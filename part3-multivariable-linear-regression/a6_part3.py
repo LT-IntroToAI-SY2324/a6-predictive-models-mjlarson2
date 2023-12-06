d265dafc9a964edce73aa27b738f8ae438b47455
mjlarson2 <mjlarson2@cps.edu>
2023-12-06 10:49:16 -0600

    finishd part 3

--- part3-multivariable-linear-regression/a6_part3.py
+++ part3-multivariable-linear-regression/a6_part3.py
@@ -5,20 +5,31 @@ from sklearn.model_selection import train_test_split
 
 #imports and formats the data
 data = pd.read_csv("part3-multivariable-linear-regression/car_data.csv")
-x = data[["miles","age"]].values
+x = data[["miles(000)","age"]].values
 y = data["Price"].values
 
 #split the data into training and testing data
 xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2)
 #create linear regression model
-model = LinearRegression.fit(xtrain, ytrain)
+model = LinearRegression().fit(xtrain, ytrain)
 #Find and print the coefficients, intercept, and r squared values. 
 #Each should be rounded to two decimal places. 
-print(np.around(model.coef_, 2))
-print(round(float(model.intercept), 2))
-print(round(model.score(x, y), 2))
+print(f'Coefficients: {np.around(model.coef_, 2)}')
+print(f'Bias: {round(float(model.intercept_), 2)}')
+print(f'R squared: {round(model.score(x, y), 2)}')
+predict = np.around(model.predict(xtest))
 
 #Loop through the data and print out the predicted prices and the 
 #actual prices
 print("***************")
 print("Testing Results")
+for i in range(len(xtest)):
+    actual = ytest[i]
+    predictedy = predict[i]
+    xcoord = xtest[i]
+    print(f"Miles(000): {xcoord[0]} Age: {xcoord[1]} Actual: {actual} Predicted: {predictedy}")
+
+
+# For the writeup - changes every time its run b/c of train-test-split
+print(np.around(model.predict([[89, 10]])))
+print(np.around(model.predict([[150, 20]])))
--- part3-multivariable-linear-regression/a6_part3_writeup.md
+++ part3-multivariable-linear-regression/a6_part3_writeup.md
@@ -5,9 +5,13 @@ After completing `a6_part3.py` answer the following questions
 ## Questions to answer
 
 1. What is the R Squared coefficient for your model? What does that mean about the model in relation to your data?
-
+The R squared coefficient is 0.86, which means that the model does a good job of explaining the variance in the data.
 2. Is your model accurate? Why or why not?
-
+This model is accurate, both because of its high R squared value, but also because of its high r value of ~0.92, which indicates
+a very strong negative correlation between the independent and dependent variables.
 3. What does the model predict a 10-year-old car with 89000 miles is worth? What about a car that is 20 years old with 150000 miles?
-
+The model predicts that the first car is worth $9257, and the second is worth $2606
 4. You may notice that some of your predicted results are negative. This is occurring when the value of age and the mileage of the car are very high. Why do you think this is happening?
+
+This may be happening because of the unbounded linear nature of the model, where, in this case, once the x-axis values go high enough, it is inevitable that the y will dip below the x-axis. Since there is no actual data for these values to reference, the linear nature
+of the model necessitates that negative value.
