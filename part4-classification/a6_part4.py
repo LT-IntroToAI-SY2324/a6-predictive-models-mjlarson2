287ae243a5d4809d668f4c312415d36fe1d210af
mjlarson2 <mjlarson2@cps.edu>
2023-12-08 10:24:40 -0600

    finished part 4

--- part4-classification/a6_part4.py
+++ part4-classification/a6_part4.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression as LR
 from sklearn.model_selection import train_test_split as tts
 from sklearn.preprocessing import StandardScaler
+import numpy as np
 
 ref = ['Yes', 'No']
 
@@ -12,18 +13,17 @@ x = data[["Age", "EstimatedSalary", "Gender"]].values
 y = data["Purchased"].values
 
 # Step 1: Print the values for x and y
-print(x)
-print(y)
+# print(x)
+# print(y)
 # Step 2: Standardize the data using StandardScaler, 
 scaler = StandardScaler().fit(x)
 # Step 3: Transform the data
-x = scaler.tranform(x)
+x = scaler.transform(x)
 # Step 4: Split the data into training and testing data
 xtrain, xtest, ytrain, ytest = tts(x, y)
 # Step 5: Fit the data
-model = LR().fit(xtrain, ytrain)
 # Step 6: Create a LogsiticRegression object and fit the data
-
+model = LR().fit(xtrain, ytrain)
 # Step 7: Print the score to see the accuracy of the model
 print(f'Accuracy: {model.score(xtest, ytest)}')
 # Step 8: Print out the actual ytest values and predicted y values
@@ -33,6 +33,8 @@ for i in range(0, len(xtest)):
     x = x.reshape(-1, 3)
     p = int(model.predict(x))
     y = ytest[i]
+    print(x)
     print(f'Predicted : {ref[p]}; Actual: {ref[y]}')
+print(model.predict(np.asarray([34, 56000, 1]).reshape(1, -1)))
     
     
--- part4-classification/a6_part4_writeup.md
+++ part4-classification/a6_part4_writeup.md
@@ -5,10 +5,13 @@ After completing `a6_part4.py` answer the following questions
 ## Questions to answer
 
 1. Comment out the StandardScaler and re-run your test. How accurate is the model? Why is that?
-
+The model is not particuarly accurate, with an r value of 0.65. This is probably because the three independent variables are
+on completely different scales from each other, throwing off the model as it cannot properly associate them all with the dependent variable at the same time.
 2. How accurate is the model with the StandardScaler? Is this model accurate enough for the given use case? Explain.
-
+The model is very accurate, with an r value of 0.89. This is accurate enough for the given use case of predicting purchases, given
+that it's just generally accurate.
 3. Looking at the predicted and actual results, how did the model do? Was there a pattern to the inputs that the model was incorrect about?
-
+The model genreally did well, and I can't identify a pattern to the inputs the model was incorrect about.
 4. Would a 34 year old Female who makes 56000 a year buy an SUV according to the model? Remember to scale the data before running it through the model.
+No.
 
